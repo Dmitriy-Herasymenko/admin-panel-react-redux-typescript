43a977989c47398c308a56f87a459c14c299a004
Dmitriy-Herasymenko <shiningxors@gmail.com>
2021-10-30 15:22:33 +0300

    create action creator and get request

--- ui/src/components/userList.tsx
+++ ui/src/components/userList.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../store/action-creator/user";
 
 export const UserList: React.FC = () => {
     const {users, error, loading} = useTypedSelector( state => state.users);
-    console.log("state", users)
+    const dispatch = useDispatch();
+    console.log("users", users)
+    useEffect(() => {
+        dispatch(fetchUsers())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <h1>{error}</h1>;
+
     return (
         <div>
-            
+            {users.map((user, index) =>
+                <div key={index}>{user.name}</div>
+            )}
         </div>
     );
 };
---
+++ ui/src/store/action-creator/user.ts
@@ -0,0 +1,20 @@
+import {Dispatch} from "react";
+import {UserAction, userActionTypes} from "../../types/users";
+import axios from "axios";
+
+
+export const fetchUsers = () => {
+    return async (dispatch: Dispatch<UserAction>) => {
+        try {
+            dispatch({type: userActionTypes.FETCH_USERS});
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            dispatch({type: userActionTypes.FETCH_USERS_SUCCESS, payload: response.data});
+        }
+        catch (e) {
+            dispatch({
+                type: userActionTypes.FETCH_USERS_ERROR,
+                payload: 'Error loading users'
+            })
+        }
+    }
+}
