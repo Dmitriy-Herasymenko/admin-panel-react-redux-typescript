e99c6271dfba7d0822ad30d2e40c16bd21ff5685
Dmitriy-Herasymenko <shiningxors@gmail.com>
2021-10-30 15:46:24 +0300

    create action useActions.ts

--- ui/src/components/userList.tsx
+++ ui/src/components/userList.tsx
@@ -2,15 +2,15 @@ import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {useDispatch} from "react-redux";
 import {fetchUsers} from "../store/action-creator/user";
+import {useActions} from "../hooks/useActions";
 
 export const UserList: React.FC = () => {
     const {users, error, loading} = useTypedSelector( state => state.users);
-    const dispatch = useDispatch();
+    const {fetchUsers} = useActions();
     console.log("users", users)
     useEffect(() => {
-        dispatch(fetchUsers())
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
+        fetchUsers()
+},[]);
 
     if (loading) return <h1>Loading...</h1>;
     if (error) return <h1>{error}</h1>;
