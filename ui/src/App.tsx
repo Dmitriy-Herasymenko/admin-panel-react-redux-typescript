6d33cc23e4282f59339f5f5bbe5e01a79e0fbe8d
Dmitriy-Herasymenko <shiningxors@gmail.com>
2021-11-01 16:40:08 +0200

    install routers and modules menu

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { ResponsiveDrawer } from "./modules/menu/index"
 import { UserList } from "./components/userList";
 import { TodoList } from "./components/todoList";
 
 function App() {
   return (
     <div className="App">
-        <UserList />
-        <TodoList />
+     <ResponsiveDrawer />
     </div>
   );
 }
