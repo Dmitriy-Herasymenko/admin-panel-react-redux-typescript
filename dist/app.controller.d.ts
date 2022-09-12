d97068b93c2708adac1d72c2ff81b006721eb753
Dmitriy Herasymenko <shiningxors@gmail.com>
2022-09-12 09:06:01 +0300

    add server where: create db, create user controller module & services and create config env

---
+++ dist/app.controller.d.ts
@@ -0,0 +1,11 @@
+import { AppService } from "./app.service";
+export declare class AppController {
+    private appService;
+    constructor(appService: AppService);
+    getUsers(): {
+        id: number;
+        name: string;
+        age: number;
+        position: string;
+    }[];
+}
---
+++ dist/app.service.d.ts
@@ -0,0 +1,8 @@
+export declare class AppService {
+    getUsers(): {
+        id: number;
+        name: string;
+        age: number;
+        position: string;
+    }[];
+}
